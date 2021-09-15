8f012638cb252a748cca617cd8da969e485c6a39
Yuhnosuk Hashimoto <48144034+Yuhnosuke@users.noreply.github.com>
2021-09-15 17:00:32 +0900

    とりあえずBaseDividerつくった

---
+++ components/atmos/BaseDivider.vue
@@ -0,0 +1,18 @@
+<template>
+    <hr class="divider">
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+    name: 'BaseDivider', 
+})
+</script>
+
+<style scoped>
+    .divider {
+        color: #333;
+    }
+
+</style>
