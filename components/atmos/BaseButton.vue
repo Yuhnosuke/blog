3d3c29a1e6d0402f9b6f1a4cbea2bb914f47b924
Yuhnosuk Hashimoto <48144034+Yuhnosuke@users.noreply.github.com>
2021-09-15 17:00:53 +0900

    とりあえずBaseButtonrつくった

---
+++ components/atmos/BaseButton.vue
@@ -0,0 +1,24 @@
+<template>
+    <button type="button">
+        {{label}}
+    </button>
+    
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+    name: 'BaseButton',
+    props: {
+        label: {
+            type: String,
+            required: true,
+        }
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
