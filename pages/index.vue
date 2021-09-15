87f2028994399ac0d42d1a748712a31a9feffce7
Yuhnosuk Hashimoto <48144034+Yuhnosuke@users.noreply.github.com>
2021-09-15 17:01:48 +0900

    index.vue でBasePostCardList呼んだ

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,14 @@
 <template>
+    <BasePostCardList />
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import BasePostCardList from '../components/organisms/BasePostCardList.vue';
 
-export default Vue.extend({})
+export default Vue.extend({
+    components: {
+        BasePostCardList,
+    }
+})
 </script>
